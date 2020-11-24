2a8fb9133f9050040ccc01b83c6994b1396eecb5
marpernas <marpernas@gmail.com>
2020-11-24 17:23:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const convert = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
+const { render } = require('ejs')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if (cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            error: false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        })
+    } else {
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
+
+})
+
+
+
+
+app.listen(3000, err => {
+    if (err) {
+        console.log('servidor não inicializado')
+    } else {
+        console.log('ConvertMyMoney esta online')
+    }
+
+})
